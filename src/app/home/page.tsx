4f1c3c687b4e350bae80e6cf0cf97c933278864e
vijayprathap <cube@genpixels.com>
2024-11-12 20:28:29 +0530

    ui fix

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,8 @@
 import Hompepage from '@/core-component/homepage/Hompepage'
 import React from 'react'
 
-const Home = () => <Hompepage />
+const Home = () => {
+    return <Hompepage />
+}
 
 export default Home
