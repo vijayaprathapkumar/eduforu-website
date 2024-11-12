b7773ccb3cbf945ca28bc06a2eef50815aa6a0f3
MohanapriyaML <mohanapriyamyilsamy@genpixels.com>
2024-11-12 12:54:17 +0530

    destination-details completed

--- src/core-component/study-destination/config/index.ts
+++ src/core-component/study-destination/config/index.ts
@@ -1,9 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { AustraliaDetails } from "./australia";
 import { CanadaDetails } from "./canada";
+import { IrelandDetails } from "./ireland";
 import { UKDetails } from "./uk";
 
-
-export const pageMap:any = {
-    uk : UKDetails ,
-    canada:CanadaDetails
-}
+export const pageMap: any = {
+  uk: UKDetails,
+  canada: CanadaDetails,
+  ireland: IrelandDetails,
+  australia: AustraliaDetails,
+};
